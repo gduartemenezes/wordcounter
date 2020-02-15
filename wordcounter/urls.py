c6546348001f42d5bb8b20f5e22ada01b1e39821
GustavoD <gduartemenezes@gmail.com>
2020-02-15 20:08:04 -0300

    Added Static files settings and folder to project, and a css stylesheet

--- wordcounter/urls.py
+++ wordcounter/urls.py
@@ -1,4 +1,7 @@
 
+
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -7,4 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('count/', views.count, name ='count'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
