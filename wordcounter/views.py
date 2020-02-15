792bf950189c8104c33f599434689fb881f74e32
GustavoD <gduartemenezes@gmail.com>
2020-02-15 18:42:17 -0300

    Added a second logic - Worddictionary

--- wordcounter/views.py
+++ wordcounter/views.py
@@ -6,7 +6,24 @@ def home(request):
 
 
 def count(request):
+    # collect the input
     fulltext = request.GET['fulltext']
+
+    # split it in a list
     wordcount = fulltext.split()
 
-    return render(request, 'count.html', {'fulltext' : fulltext, 'count' : len(wordcount) })
+    #Counts how many time each word appears
+    word_dictionary = {}
+    for word in wordcount:
+        if word in word_dictionary:
+            #increase
+            word_dictionary[word] += 1
+        else:
+            #add to dictionary
+            word_dictionary[word] = 1
+
+        
+
+    return render(request, 'count.html',
+         {'fulltext' : fulltext, 'count' : len(wordcount),
+          'word_dict': word_dictionary.items()})
