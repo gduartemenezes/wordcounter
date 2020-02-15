12f3a3b9c8274cb9e884ef93ea794f5b6d44dd4c
GustavoD <gduartemenezes@gmail.com>
2020-02-15 15:31:55 -0300

    Added count url, view  and template, added counting logic to views.count

--- wordcounter/views.py
+++ wordcounter/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordcount = fulltext.split()
+
+    return render(request, 'count.html', {'fulltext' : fulltext, 'count' : len(wordcount) })
