3f26a9025b340977b6b65267a6ed45de4cf12de9
GustavoD <gduartemenezes@gmail.com>
2020-02-17 09:55:58 -0300

    Version 2: changed countwords func to sort the most appeared word

--- wordcounter/views.py
+++ wordcounter/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+import operator
 def home(request):
     return render(request, 'home.html')
 
@@ -22,8 +22,10 @@ def count(request):
             #add to dictionary
             word_dictionary[word] = 1
 
+    sortedwords =  sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse = True)
+
         
 
     return render(request, 'count.html',
          {'fulltext' : fulltext, 'count' : len(wordcount),
-          'word_dict': word_dictionary.items()})
+          'sortedwords': sortedwords})
